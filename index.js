d0340ef4479a6df9daee581c4ec91ab5e4432226
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-04-13 22:16:56 +0600

    cors added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const phones = require('./phones.json');
+const cors = require('cors');
 
 const app = express();
 const port = 5000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('my phone information is coming soon- nodemon-my');
